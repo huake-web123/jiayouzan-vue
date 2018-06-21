ad9b3d72ef07c9901bc9055007bf99a4c373bfda
smilehanxiao <413169485@qq.com>
2018-06-21 17:15:39 +0800

    关于swiper的用法

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,32 @@
+<!--Created by hjx on 2018/6/21.-->
+<template>
+    <div class="wrapper-box">
+      <swiper-slide v-for="banner in banners">
+        <img class="banner-bg" :src="banner">
+      </swiper-slide>
+    </div>
+</template>
+
+<script>
+  import 'swiper/dist/css/swiper.css'
+  import {swiper,swiperSlide} from 'vue-awesome-swiper'
+export default {
+  name: '',
+  data() {
+    return {
+      banners: ['http://7xqnxu.com1.z0.glb.clouddn.com/jiayouzan-vue-1.jpg',
+        'http://7xqnxu.com1.z0.glb.clouddn.com/jiayouzan-vue-2.jpg',
+        'http://7xqnxu.com1.z0.glb.clouddn.com/jiayouzan-vue-3.jpg',
+        'http://7xqnxu.com1.z0.glb.clouddn.com/jiayouzan-vue-4.jpg']
+    }
+    components: {
+      'swiper': swiper,
+      'swiper-slide': swiperSlide
+    }
+  }
+}
+</script>
+
+<style scoped>
+ /*@import "../../assets/style/swiper.min.css";*/
+</style>
