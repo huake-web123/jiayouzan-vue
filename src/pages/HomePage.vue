ce131c01ecbe51b522d32e189e1eb7b165acc502
smilehanxiao <413169485@qq.com>
2018-06-21 17:35:05 +0800

    -m修改组件位置

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -8,25 +8,24 @@
 </template>
 
 <script>
-  import 'swiper/dist/css/swiper.css'
-  import {swiper,swiperSlide} from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
+import {swiper,swiperSlide} from 'vue-awesome-swiper'
 export default {
   name: '',
-  data() {
+  data () {
     return {
       banners: ['http://7xqnxu.com1.z0.glb.clouddn.com/jiayouzan-vue-1.jpg',
         'http://7xqnxu.com1.z0.glb.clouddn.com/jiayouzan-vue-2.jpg',
         'http://7xqnxu.com1.z0.glb.clouddn.com/jiayouzan-vue-3.jpg',
         'http://7xqnxu.com1.z0.glb.clouddn.com/jiayouzan-vue-4.jpg']
     }
-    components: {
-      'swiper': swiper,
-      'swiper-slide': swiperSlide
-    }
+  },
+  components: {
+    'swiper': swiper,
+    'swiper-slide': swiperSlide
   }
 }
 </script>
 
 <style scoped>
- /*@import "../../assets/style/swiper.min.css";*/
 </style>
