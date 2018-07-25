007bcc1a800cda224a4c1230d4d06e8b22648df5
smilehanxiao <413169485@qq.com>
2018-07-26 01:32:04 +0800

    取token

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -13,18 +13,48 @@
             <input type="password" v-model="passWord">
           </div>
         </div>
-        <div class="login-box" :class="{selected:userName&&passWord !== ''}">登录</div>
+        <div class="login-box" :class="{selected:userName&&passWord !== ''}"
+        @click="entry()">登录</div>
       </div>
     </div>
 </template>
 
 <script>
+import Cookies from 'js-cookie'
 export default {
   name: '',
   data () {
     return {
       userName: '',
       passWord: ''
+    }
+  },
+  mounted () {
+    this.$nextTick(() => {
+      this.isJump()
+    })
+  },
+  methods: {
+    isJump () {
+      if (Cookies.get('token') !== undefined) {
+        console.log(Cookies.get('token'))
+        this.$router.push({path: '/HomePage'})
+      } else {
+      }
+    },
+    entry () {
+      this.$ajax({
+        method: 'post',
+        url: 'https://dsn.apizza.net/mock/fb275314bc53ebc54f45a6b698d2433d/login',
+        data: JSON.stringify({
+          account: this.userName,
+          password: this.passWord
+        })
+      }).then((res) => {
+        console.log(res.data.token)
+        Cookies.set('token', res.data.token, { expires: 7, path: '' })
+        this.$router.push({path: '/HomePage'})
+      })
     }
   }
 }
